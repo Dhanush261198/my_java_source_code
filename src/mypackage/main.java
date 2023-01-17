dae361cd780d7bf4b566679d4846ad9e8b1d1ffc
Dhanush261198 <yesyoyo420@gmail.com>
2023-01-17 12:29:27 +0530

    my repo

---
+++ src/mypackage/main.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+		employee emp = new employee;
+		
+		emp.intrest
+	}
+
+}
---
+++ src/mypackage/poly.java
@@ -0,0 +1,31 @@
+package mypackage;
+
+public class poly {
+	
+	public void intrest(float amount)
+	{
+		System.out.print("Amount");
+	}
+	public void intrest(float pricipal, float rate , float time)
+	{
+		System.out.print("Amount");
+	}
+	public void intrest(float pricipal, float total_intrest)
+	{
+		System.out.print("Amount");
+		
+	}
+public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+		bank empl = new bank();
+		
+		empl.intrest(1000);
+		empl.intrest(1000,1,1);
+		empl.intrest(1000,1);
+	}
+	
+	}
+
+
